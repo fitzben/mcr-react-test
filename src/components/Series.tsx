87f2de6678b13899fafa0b9840f56f13d94ee7c9
Benjamin Sitompul <benjamin@rubyh.co>
2021-05-03 00:30:52 +0700

    create series page

--- src/components/Series.tsx
+++ src/components/Series.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
-
+import axios from 'axios';
+import { dummy } from './DummyData';
+import { Col, Container, Row } from 'react-bootstrap';
+import { orderBy } from 'lodash';
+import Header from './Header';
+import { Title, TtileHome } from './Homepage'
+import Footer from './Footer';
 
 export default function Series() {
+  const url = 'localhost:3000/sample.json'
+  const data = axios.get(url).then(res => console.log(res))
+  
+  const datas: any = orderBy(dummy.entries.filter((data) => data.releaseYear >= 2010 && data.programType === 'series'), ['title'], ['asc']).slice(0, 21)
+
   return (
-    <div>INI SERIES</div>
+    <Container fluid>
+      <Header />
+      <TtileHome>
+        <Title>
+          Popular Series
+        </Title>
+      </TtileHome>
+      <SeriesData datas={datas} />
+      <Footer />
+    </Container>
+  )
+}
+
+function SeriesData({datas}: any) {
+  return (
+    <Row className="mt-5">
+      {datas.map((data: any) => {
+        return (
+          <Col md={2} className="mb-5">
+            <img src={data?.images?.PosterArt?.url} alt="" width='185px' height='265px' />
+            <div>{data?.title}</div>
+          </Col>
+        )
+      })}
+    </Row>
   )
 }
